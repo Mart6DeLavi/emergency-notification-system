6597401d5511fb011b1a10189febddc05104425d
martix_de_lavi <azmater59034@gmail.com>
2026-08-01 17:43:23 +0200

    fix: kafka communication between authentication-service and user-management-service

--- user-management-service/src/main/java/com/sensa/usermanagementservice/config/kafka/UserDataKafkaConfig.java
+++ user-management-service/src/main/java/com/sensa/usermanagementservice/config/kafka/UserDataKafkaConfig.java
@@ -1,5 +1,6 @@
 package com.sensa.usermanagementservice.config.kafka;
 
+import com.sensa.usermanagementservice.dto.UserCreateEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -36,19 +40,21 @@ public class UserDataKafkaConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public DefaultKafkaConsumerFactory<String, UserCreateEvent> userCreateEventConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.sensa.usermanagementservice.dto");
-        return new DefaultKafkaConsumerFactory<>(props);
+
+        JsonDeserializer<UserCreateEvent> deserializer = new JsonDeserializer<>(UserCreateEvent.class);
+        deserializer.setUseTypeHeaders(false);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, UserCreateEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, UserCreateEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(userCreateEventConsumerFactory());
         return factory;
     }
 }
